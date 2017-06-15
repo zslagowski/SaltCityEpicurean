e2a0e69a4c77c7c0b2e4597c93fa550a49d8d5e3
Zach <Zach>
2017-06-15 15:49:11 -0600

    Added original GEMM _S started theme to the repository as a base theme

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package GEMM_Framework
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gemm-framework' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'gemm-framework' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'gemm-framework' ), 'gemm-framework', '<a href="https://automattic.com/" rel="designer">Zach Slagowski</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
